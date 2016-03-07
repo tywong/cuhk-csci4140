5c0f0745391b02c92e026eaee1cfb496c28e7b7e
WONG Tsz Yeung <tszyeung@gmail.com>
2016-03-08 03:32:53 +0800

    added nodejs example on singleton

---
+++ node-singleton-example/Counter.js
@@ -0,0 +1,21 @@
+var Counter = (function() {
+  var privateCounter = 0;
+
+  function changeBy(val) {
+    privateCounter += val;
+  }
+
+  return {
+    increment: function() {
+      changeBy(1);
+    },
+    decrement: function() {
+      changeBy(-1);
+    },
+    value: function() {
+      return privateCounter;
+    }
+  }
+})();
+
+module.export = Counter;
